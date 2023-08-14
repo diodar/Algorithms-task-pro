c94c9f74b3f7ad8f4a0fd292dbc6c83321195757
diodar <mikhailgalis@gmail.com>
2023-08-14 10:59:50 +0300

    Little fix 'FOR' cycle

--- src/app/services/DataService.java
+++ src/app/services/DataService.java
@@ -9,9 +9,9 @@ public class DataService {
     // Алгоритм лінійного пошуку
     public List<Integer> search(List<Product> list, double x) {
         List<Integer> indexList = new ArrayList<>();
-        for (int i = 0; i < list.toArray().length; i++)
-            if (list.get(i).getPrice() == x)
-                indexList.add(i);
+        for (int i = 0; i < list.toArray().length; i++) {
+            if (list.get(i).getPrice() == x) indexList.add(i);
+        }
                 return indexList;
     }
 }
